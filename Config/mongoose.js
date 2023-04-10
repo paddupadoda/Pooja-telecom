1aa4a27dac9d1fe81d2432e908c7a215b92628fa
Harsh Patel <yoharsh113@gmail.com>
2023-04-10 16:30:04 +0530

    filler

--- Config/mongoose.js
+++ Config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sheryianscodingschool:sheryians@cluster0.imx2n.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://badipyaarihai:entersportslenevo@pooja-telecom-cluster.dxgw0n7.mongodb.net/?retryWrites=true&w=majority')
 
 const db = mongoose.connection;
 
